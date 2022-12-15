e7f0cb14c895f15136b3ad57182b678cc5822b6f
MaxKov86 <max.kov86@ukr.net>
2022-12-15 20:38:04 +0300

    Create refs.js and added code

---
+++ src/refs.js
@@ -0,0 +1,9 @@
+const countryInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+export default {
+  countryInput,
+  countryList,
+  countryInfo,
+};
